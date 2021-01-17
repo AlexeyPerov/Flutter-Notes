ea38b6bb034982bde14538996644c02211ba06ca
Alexey Perov <alvperov@gmail.com>
2021-01-17 18:11:26 +0300

    Stub create & edit functionality added

---
+++ lib/screens/note_edit/note_edit_screen.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
+import 'package:mynotes/redux/models/note.dart';
+
+class NoteEditScreen extends StatefulWidget {
+  final Note note;
+  final Function(String, String) onCreate;
+  final Function(String, String, String) onUpdate;
+  final VoidCallback onPop;
+
+  NoteEditScreen({
+    Key key,
+    this.note,
+    this.onCreate,
+    this.onUpdate,
+    this.onPop,
+  }) : super(key: key);
+
+  @override
+  _NoteEditScreenState createState() => _NoteEditScreenState();
+}
+
+class _NoteEditScreenState extends State<NoteEditScreen> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  String _title = '';
+  String _contents = '';
+
+  bool _validateAndSave() {
+    final form = _formKey.currentState;
+
+    if (form.validate()) {
+      form.save();
+      return true;
+    }
+
+    return false;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Note'),
+        actions: <Widget>[
+          FlatButton(
+            textColor: Colors.white,
+            onPressed: () {
+              if (_validateAndSave()) {
+                if (widget.note != null && widget.note.id.isNotEmpty) {
+                  widget.onUpdate(
+                    widget.note.id,
+                    _title,
+                    _contents,
+                  );
+                } else {
+                  widget.onCreate(_title, _contents);
+                }
+                widget.onPop();
+              }
+            },
+            child: Text("SAVE"),
+            key: Key("bt_save"),
+            shape: CircleBorder(side: BorderSide(color: Colors.transparent)),
+          ),
+        ],
+      ),
+      body: Form(
+        key: _formKey,
+        child: Container(
+          child: Column(
+            children: [
+              TextFormField(
+                maxLines: 1,
+                keyboardType: TextInputType.text,
+                autofocus: true,
+                onSaved: (value) => _title = value,
+                initialValue: widget.note?.title,
+              ),
+              TextFormField(
+                maxLines: 3,
+                keyboardType: TextInputType.text,
+                autofocus: true,
+                onSaved: (value) => _contents = value,
+                initialValue: widget.note?.contents,
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/screens/notes/notes_screen.dart
+++ lib/screens/notes/notes_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/foundation.dart';
 import 'package:mynotes/redux/models/note.dart';
+import 'package:mynotes/screens/note_edit/note_edit_screen.dart';
 
 class NotesScreen extends StatelessWidget {
   final List<Note> noteList;
@@ -33,7 +34,7 @@ class NotesScreen extends StatelessWidget {
               padding: const EdgeInsets.all(8.0),
               itemCount: noteList.length,
               itemBuilder: (BuildContext context, int index) =>
-                  _buildCard(context, noteList[index]),
+                  _buildNoteCard(context, noteList[index]),
             ),
           ),
         ],
@@ -46,70 +47,64 @@ class NotesScreen extends StatelessWidget {
   }
 
   void _navigateToCreateEditPage(BuildContext context, {Note note}) {
-    /*Navigator.push(
+    Navigator.push(
       context,
       MaterialPageRoute(
-        builder: (context) => TodosCreateEditPage(
-          todo: todo,
+        builder: (context) => NoteEditScreen(
+          note: note,
           onCreate: onCreate,
           onUpdate: onUpdate,
           onPop: onPop,
         ),
       ),
-    );*/
+    );
   }
 
-  Dismissible _buildCard(BuildContext context, Note note) => Dismissible(
-    direction: DismissDirection.endToStart,
-    background: Card(
-      elevation: 8.0,
-      child: Container(
-        color: Colors.red,
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.end,
-          children: <Widget>[
-            Padding(
-              padding: EdgeInsets.only(right: 15.0),
-              child: Text(
-                'Remove',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 15,
+  Dismissible _buildNoteCard(BuildContext context, Note note) => Dismissible(
+        direction: DismissDirection.endToStart,
+        background: Card(
+          elevation: 8.0,
+          child: Container(
+            color: Colors.red,
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.only(right: 15.0),
+                  child: Text(
+                    'Remove',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 15,
+                    ),
+                    textAlign: TextAlign.end,
+                  ),
                 ),
-                textAlign: TextAlign.end,
-              ),
+              ],
             ),
-          ],
-        ),
-      ),
-    ),
-    key: Key(UniqueKey().toString()),
-    onDismissed: (direction) => onRemove(note.id),
-    child: Card(
-      elevation: 8.0,
-      child: InkWell(
-        /*onTap: () => _navigateToCreateEditPage(
-          context,
-          todo: note,
-        ),*/
-        child: Container(
-          padding: EdgeInsets.all(10.0),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
-              Text(note.title),
-              Checkbox(
-                value: note.archived,
-                onChanged: (value) => onArchive(
-                  note.id,
-                  !note.archived
-                ),
-              ),
-            ],
           ),
         ),
-      ),
-    ),
-  );
+        key: Key(UniqueKey().toString()),
+        onDismissed: (direction) => onRemove(note.id),
+        child: Card(
+          elevation: 8.0,
+          child: InkWell(
+            onTap: () => _navigateToCreateEditPage(context, note: note),
+            child: Container(
+              padding: EdgeInsets.all(10.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Text(note.title),
+                  Checkbox(
+                    value: note.archived,
+                    onChanged: (value) => onArchive(note.id, !note.archived),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      );
 }
